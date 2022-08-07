222cffb040eeb6ff53331641cc9bb0ae2a1f4d62
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-08-07 15:07:49 -0300

    OK

---
+++ README.txt
@@ -0,0 +1,13 @@
+// Complete as funções do ToDo
+
+
+// Ao adicionar uma tarefa o seu input deve ser limpo
+
+
+// Seu ToDo deve ter uma função para adicionar tarefa
+
+
+// Seu ToDo deve ter uma função para remover tarefa
+
+
+// Seu ToDo deve adicionar tarefas com o click no button e também com a tecla ENTER
--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: black;
+  }
+`
+
+const ListasTarefas = styled.div`
+`
+
+export default class App extends Component{
+
+  state = {
+    tarefas: "",
+    listaTarefas: []
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      tarefas: event.target.value
+    })
+  }
+
+  add = () => {
+    if (this.state.tarefas !== ""){
+      this.setState({
+        listaTarefas: this.state.listaTarefas.concat({
+          tarefas: this.state.tarefas,
+          id: Date.now()
+        }),
+        tarefas: ""
+      })
+    }
+  }
+
+  remover = (id) => {
+    this.setState({
+      listaTarefas: this.state.listaTarefas.filter((item) => item.id !== id)
+    })
+  }
+
+  enter = (event) => {
+    if (this.state.tarefas.length > 0 && event.key === "Enter"){
+      this.setState({
+        listaTarefas: this.state.listaTarefas.concat({
+          tarefas: this.state.tarefas,
+          id: Date.now()
+        }),
+        tarefas: ""
+      })
+    }
+  }
+
+  render(){
+    return(
+      <ListasTarefas>
+        <input
+          onChange={this.handleChange}
+          value={this.state.tarefas}
+          onKeyPress={this.enter} />
+        <button onClick={this.add}>Adicionar</button>
+        {this.state.listaTarefas.map((item) => (
+          <div key={item.id}>
+            <ul>
+              <li>{item.tarefas}</li>
+            </ul>
+            <button onClick={() => this.remover(item.id)}>x</button>
+          </div>
+        ))}
+      </ListasTarefas>
+    )
+  }
 }
-
-export default App;
