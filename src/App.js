18ded1e6d7bd1f8ec56d3592dd6afcefec61d73b
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-08-08 12:43:16 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -3,16 +3,70 @@ import styled from 'styled-components'
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
   *{
-    margin: 0;
+    font-family: 'Indie Flower', cursive;
+    margin: 10px;
     padding: 0;
     box-sizing: border-box;
-    background-color: black;
+  }
+
+  body{
+    background-color: #0ffff8;
   }
 `
 
 const ListasTarefas = styled.div`
+  background-color: #e5e66b;
+  margin: 0 auto;
+  width: 500px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 10px;
+  border: 1px solid black;
+
+  h1{
+    margin-bottom: 30px;
+  }
 `
+
+const ConteinerList = styled.div`
+  input{
+    width: 200px;
+    height: 30px;
+    border-radius: 5px;
+    border: 1px solid black;
+  }
+
+  button{
+    width: 120px;
+    height: 30px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+`
+
+const Resultado = styled.div`
+  display: flex;
+  align-items: center;
+
+  li{
+    list-style-type: none;
+  }
+
+  button{
+    width: 40px;
+    height: 30px;
+    background: red;
+    border-radius: 5px;
+    border: 1px solid black;
+  }
+
+`
+
 
 export default class App extends Component{
 
@@ -60,18 +114,22 @@ export default class App extends Component{
   render(){
     return(
       <ListasTarefas>
+         <GlobalStyle />
+         <h1>Lista</h1>
+        <ConteinerList>
         <input
           onChange={this.handleChange}
           value={this.state.tarefas}
           onKeyPress={this.enter} />
         <button onClick={this.add}>Adicionar</button>
+        </ConteinerList>
         {this.state.listaTarefas.map((item) => (
-          <div key={item.id}>
+          <Resultado key={item.id}>
             <ul>
               <li>{item.tarefas}</li>
             </ul>
             <button onClick={() => this.remover(item.id)}>x</button>
-          </div>
+          </Resultado>
         ))}
       </ListasTarefas>
     )
